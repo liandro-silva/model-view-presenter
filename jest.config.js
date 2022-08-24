7dcb1a5e80d77aaa59c367f45c52c8dbba2b5254
Liandro Silva <liandro.silva1012@gmail.com>
2022-08-24 14:32:59 -0300

    chore: add react configs

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
   testPathIgnorePatterns: [
     '<rootDir>/node_modules/'
   ],
-  // testEnvironment: 'jsdom',
+  testEnvironment: 'jsdom',
   transform: {
     '.+\\.(ts|tsx)$': 'ts-jest'
   },
