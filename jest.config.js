d90e3706e23b093f8cc8aeb90406f18ab90f2226
Liandro Silva <liandro.silva1012@gmail.com>
2022-08-24 16:02:29 -0300

    chore: config sass

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.{ts,tsx}'
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!**/*.d.ts'
   ],
   coverageDirectory: 'coverage',
   // setupFilesAfterEnv: ['<rootDir>/src/main/config/jest-setup.ts'],
