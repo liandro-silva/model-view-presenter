eebce68b226dadd7b4a7b42935176d4dbeb66a0b
Liandro Silva <liandro.silva1012@gmail.com>
2022-08-25 19:13:33 -0300

    refactor: move logo to a separate file

--- jest.config.js
+++ jest.config.js
@@ -14,7 +14,6 @@ module.exports = {
     '.+\\.(ts|tsx)$': 'ts-jest'
   },
   moduleNameMapper: {
-    '@/tests/(.*)': '<rootDir>/tests/$1',
     '@/(.*)': '<rootDir>/src/$1',
     '\\.scss$': 'identity-obj-proxy'
   }
---
+++ src/presentation/components/index.ts
@@ -0,0 +1,4 @@
+import Spinner from "./spinner";
+import Logo from "./logo";
+
+export { Logo, Spinner };
