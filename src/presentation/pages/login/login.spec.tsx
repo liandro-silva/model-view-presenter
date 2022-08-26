9b2764e0c7272c5f33a0c6fd883ab6d0ca3379c5
Liandro Silva <liandro.silva1012@gmail.com>
2022-08-25 21:43:34 -0300

    test: ensure Login starts with submit button disabled

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -4,17 +4,21 @@ import Login from ".";
 
 const makeSut = () => {
   const sut = () => render(<Login />);
+
   return {
     sut,
   };
 };
 
 describe("\n Page - Login \n", () => {
-  it("should not initially render the children of form status", () => {
+  it("should start with initial states", () => {
     const { sut } = makeSut();
     sut();
+
     const errorWrap = screen.getByTestId("error-wrap");
+    const submitButton = screen.getByTestId("submit") as HTMLButtonElement;
 
     expect(errorWrap.childElementCount).toBe(0);
+    expect(submitButton.disabled).toBe(true);
   });
 });
