178322f8555cc01def944aff8399c9a3447fbb36
Liandro Silva <liandro.silva1012@gmail.com>
2022-09-03 16:55:48 -0300

    feat: ensure Login add accessToken to localstorage on success

--- src/presentation/mocks/authentication.mock.ts
+++ src/presentation/mocks/authentication.mock.ts
@@ -9,6 +9,6 @@ export class AuthenticationSpy implements Authentication {
   async execute(params: AuthenticationParams): Promise<AccountModel> {
     this.params = params;
     this.callsCount++;
-    return Promise.resolve(this.account);
+    return this.account;
   }
 }
