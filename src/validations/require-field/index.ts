747e6d5db3dbe1a2fbe2f97a88d2c250d9763950
Liandro Silva <liandro.silva1012@gmail.com>
2022-09-03 18:51:00 -0300

    feat: ensure RequiredFIeldValidation returns error if field is empty

---
+++ src/validations/require-field/index.ts
@@ -0,0 +1,9 @@
+import { FieldValidation } from "@/validations/protocols";
+import { RequiredFieldError } from "@/validations/errors";
+
+export class RequiredFieldValidation implements FieldValidation {
+  constructor(readonly field: string) {}
+  validate(value: string): Error {
+    return new RequiredFieldError();
+  }
+}
