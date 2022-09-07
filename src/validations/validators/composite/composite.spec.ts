80ed0b151d0f30b8ffe920b813f1aa902968a195
Liandro Silva <liandro.silva1012@gmail.com>
2022-09-06 22:03:09 -0300

    feat: ensure ValidationComposite returns the first error found

--- src/validations/validators/composite/composite.spec.ts
+++ src/validations/validators/composite/composite.spec.ts
@@ -4,13 +4,14 @@ import { ValidationComposite } from ".";
 describe("\n Validators - ValidationComposite \n", () => {
   it("should return error if any validation fails", () => {
     const fieldValidationSpy = new FieldValidationSpy("any_field");
+    fieldValidationSpy.error = new Error("first_error");
     const fieldValidationSpy2 = new FieldValidationSpy("any_field");
-    fieldValidationSpy2.error = new Error("any_message");
+    fieldValidationSpy2.error = new Error("second_error");
     const sut = new ValidationComposite([
       fieldValidationSpy,
       fieldValidationSpy2,
     ]);
     const error = sut.validate("any_field", "any_value");
-    expect(error).toBe("any_message");
+    expect(error).toBe("first_error");
   });
 });
