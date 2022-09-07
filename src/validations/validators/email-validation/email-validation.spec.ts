7530e4597b0c855565080986d2e6d8c1ce40feb6
Liandro Silva <liandro.silva1012@gmail.com>
2022-09-06 21:02:59 -0300

    test: ensure EmailValidation returns falsy if email is valid

--- src/validations/validators/email-validation/email-validation.spec.ts
+++ src/validations/validators/email-validation/email-validation.spec.ts
@@ -1,6 +1,7 @@
 import { EmailValidation } from ".";
 
 import { InvalidFieldError } from "@/validations/errors";
+import { faker } from "@faker-js/faker";
 
 const makeSut = () => {
   const sut = new EmailValidation("email");
@@ -12,7 +13,13 @@ const makeSut = () => {
 describe("\n Validators - Email Validation \n", () => {
   it("should return error if email is invalid", () => {
     const { sut } = makeSut();
-    const error = sut.validate("");
+    const error = sut.validate(faker.lorem.word());
     expect(error).toEqual(new InvalidFieldError());
+  });
+
+  it("should return falsy if email is valid", () => {
+    const { sut } = makeSut();
+    const error = sut.validate(faker.internet.email());
+    expect(error).toBeFalsy();
   });
 });
