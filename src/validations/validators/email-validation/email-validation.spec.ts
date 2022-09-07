a587d50bcebbc82fe720e6b2878fe77a210dea89
Liandro Silva <liandro.silva1012@gmail.com>
2022-09-06 21:43:59 -0300

    fix: ensure EmailValidation allows empty value

--- src/validations/validators/email-validation/email-validation.spec.ts
+++ src/validations/validators/email-validation/email-validation.spec.ts
@@ -22,4 +22,10 @@ describe("\n Validators - Email Validation \n", () => {
     const error = sut.validate(faker.internet.email());
     expect(error).toBeFalsy();
   });
+
+  it("should return falsy if email is empty", () => {
+    const { sut } = makeSut();
+    const error = sut.validate("");
+    expect(error).toBeFalsy();
+  });
 });
