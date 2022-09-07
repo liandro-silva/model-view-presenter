0cbb0c4aa1591f511618c64a6482061fad6df581
Liandro Silva <liandro.silva1012@gmail.com>
2022-09-06 22:09:30 -0300

    refactor: move sut creation to a factory method

--- src/validations/validators/composite/composite.spec.ts
+++ src/validations/validators/composite/composite.spec.ts
@@ -1,16 +1,30 @@
 import { FieldValidationSpy } from "@/validations/validators/mocks";
 import { ValidationComposite } from ".";
 
+type SutProps = {
+  sut: ValidationComposite;
+  fieldValidationsSpy: FieldValidationSpy[];
+};
+
+const makeSut = (): SutProps => {
+  const fieldValidationsSpy = [
+    new FieldValidationSpy("any_field"),
+    new FieldValidationSpy("any_field"),
+  ];
+  const sut = new ValidationComposite(fieldValidationsSpy);
+  return {
+    sut,
+    fieldValidationsSpy,
+  };
+};
+
 describe("\n Validators - ValidationComposite \n", () => {
   it("should return error if any validation fails", () => {
-    const fieldValidationSpy = new FieldValidationSpy("any_field");
-    fieldValidationSpy.error = new Error("first_error");
-    const fieldValidationSpy2 = new FieldValidationSpy("any_field");
-    fieldValidationSpy2.error = new Error("second_error");
-    const sut = new ValidationComposite([
-      fieldValidationSpy,
-      fieldValidationSpy2,
-    ]);
+    const { sut, fieldValidationsSpy } = makeSut();
+
+    fieldValidationsSpy[0].error = new Error("first_error");
+    fieldValidationsSpy[1].error = new Error("first_error");
+
     const error = sut.validate("any_field", "any_value");
     expect(error).toBe("first_error");
   });
