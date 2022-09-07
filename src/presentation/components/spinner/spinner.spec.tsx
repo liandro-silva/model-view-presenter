942e822b135dc87a3f30cf55ecef381a4a083cae
Liandro Silva <liandro.silva1012@gmail.com>
2022-09-07 09:56:16 -0300

    test: ensure spinner loads with negativeClass

---
+++ src/presentation/components/spinner/spinner.spec.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Spinner from ".";
+
+describe("\n Components - Spinner\n", () => {
+  it("should render with negativeClass", () => {
+    render(<Spinner isNegative={true} />);
+    const loading = screen.getByTestId("loading") as HTMLDivElement;
+    expect(loading.classList).toContain("negative");
+  });
+
+  it("should render without negativeClass", () => {
+    render(<Spinner isNegative={false} />);
+    const loading = screen.getByTestId("loading") as HTMLDivElement;
+    expect(loading.classList).not.toContain("negative");
+  });
+});
