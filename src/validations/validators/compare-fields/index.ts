9407fa1e2c1c248494bcf9c3f9a90f4fccfea0da
Liandro Silva <liandro.silva@outlook.com>
2022-09-23 22:50:17 -0300

    feat: ensure CompareFieldsValidation returns falsy if validation succeeds

--- src/validations/validators/compare-fields/index.ts
+++ src/validations/validators/compare-fields/index.ts
@@ -4,6 +4,6 @@ import { InvalidFieldError } from '@/validations/errors'
 export class CompareFieldsValidation implements FieldValidation {
   constructor (readonly field: string, private readonly valueToCompare: string) {}
   validate (value: string): Error {
-    return new InvalidFieldError()
+    return value !== this.valueToCompare ? new InvalidFieldError() : null
   }
 }
