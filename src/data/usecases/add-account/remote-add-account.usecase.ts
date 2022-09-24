870cddcb48d8271f92aa6c4e7fcda8b744fd47d0
Liandro Silva <liandro.silva@outlook.com>
2022-09-23 21:49:46 -0300

    feat: ensure RemoteAddAccount calls HttpPostClient with correct URL

---
+++ src/data/usecases/add-account/remote-add-account.usecase.ts
@@ -0,0 +1,30 @@
+import { HttpPostClient, HttpStatusCode } from '@/data/protocols/http'
+
+import { AddAccount, AddAccountParams } from '@/domain/usecases'
+import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors'
+import { AccountModel } from '@/domain/models'
+
+export class RemoteAddAccount implements AddAccount {
+  constructor (
+    private readonly url: string,
+    private readonly httpPostClient: HttpPostClient<
+    AddAccountParams,
+    AccountModel
+    >
+  ) {}
+
+  async execute (params: AddAccountParams): Promise<AccountModel> {
+    const httpResponse = await this.httpPostClient.post({
+      url: this.url,
+      body: params
+    })
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok:
+        return httpResponse.body
+      case HttpStatusCode.unauthorized:
+        throw new InvalidCredentialsError()
+      default:
+        throw new UnexpectedError()
+    }
+  }
+}
